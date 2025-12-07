d5608aa023d849ccee364131740829819643a05a
Emmanuel Sunday <emas08177@gmail.com>
2025-12-07 17:36:44 +0100

    x1.1.8

--- src/script/utils/supabaseClient.js
+++ src/script/utils/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL || "https://pyslfvzwcmigrryoakpc.supabase.co";
 const supabaseKey = import.meta.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5c2xmdnp3Y21pZ3JyeW9ha3BjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MjYzOTAsImV4cCI6MjA2ODUwMjM5MH0.Jlb4N79HMFRgnuWcsp7FtgTNB0WQ1Lzb4NIEh79jnJs";
